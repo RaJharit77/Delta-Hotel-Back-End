c0ff4fc6028edff1f740f15b284cf3eb29db536a
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-08 23:51:58 +0300

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import cors from 'cors';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
