1536a885f07e0388d78392d978eee9b8bc389d99
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-24 13:28:46 +0300

    feat: updating server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import alasql from 'alasql';
 import cors from 'cors';
 import express from 'express';
 import fs from 'fs/promises';
@@ -85,14 +84,7 @@ app.use((req, res, next) => {
 app.get('/api/services', async (req, res) => {
     try {
         const data = await fs.readFile(path.join(__dirname, './data/data.json'), 'utf8');
-        const services = JSON.parse(data);
-        const chambres = alasql('SELECT * FROM ?', [services[0].chambres]);
-        const conciergeries = alasql('SELECT * FROM ?', [services[0].conciergeries]);
-        const spaCards = alasql('SELECT * FROM ?', [services[0].spaCards]);
-        const autresServices = alasql('SELECT * FROM ?', [services[0].autresServices]);
-
-        console.log('Données extraites:', { chambres, autresServices, spaCards, conciergeries });
-        res.json({ chambres, autresServices, spaCards, conciergeries });
+        res.json(JSON.parse(data));
     } catch (err) {
         console.error('Error reading data.json:', err);
         res.status(500).json({ message: 'Internal server error', error: err.message });
