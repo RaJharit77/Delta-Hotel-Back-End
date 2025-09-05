b12631d04517ef5361d1cfff90bfd80f413fdf2f
RaJharit77 <hei.rajo.2@gmail.com>
2025-09-05 14:29:00 +0300

    chore: Update prisma.js

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/index.js'
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis;
