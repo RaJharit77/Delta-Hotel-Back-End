00d53e9608bc1deca4a5f6edd225e08a2ed7ff0e
RaJharit77 <hei.rajo.2@gmail.com>
2025-09-05 13:16:37 +0300

    chore: Update prisma.js file

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,8 +1,8 @@
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis;
 
 export const prisma = globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
